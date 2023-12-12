db0ce2ca08c35d5ed62219afd9e3d3b8c28e2776
mahirficent <mahirkhan0397@gmail.com>
2023-12-12 15:56:29 +0600

    yoo Task Tracker version 1.0.0

---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+        <h1>Task Tracker</h1>
+
+        <Button 
+            v-show="homePage"
+            @btn-click="$emit('toggle-add-task')"
+            :text="showAddTask ? 'Close' : 'Add Task'" 
+            :color="showAddTask ? 'Red' : 'Blue' "/>
+
+    </header>
+</template>
+
+
+
+<script>
+    import Button from './Buttons.vue'
+
+    export default {
+        name: 'Header',
+
+        props: {
+            showAddTask: Boolean
+        },
+
+        components: {
+            Button
+        },
+
+        computed: {
+            homePage() {
+                if(this.$route.path === '/') {
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+    }
+
+</script>
+
+
+
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        margin-bottom: 20px;
+    }
+
+</style>
